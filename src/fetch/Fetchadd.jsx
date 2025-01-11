da5b103e62e1f0ae904deab8bfbb14daea3a7bd9
Yiğit Bayır <yitaris>
2025-01-11 05:04:30 +0300

    heydostum

--- src/fetch/Fetchadd.jsx
+++ src/fetch/Fetchadd.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useSupabase } from "../context/SupabaseContext";
 import { ubgida } from "../assets";
-
+import { motion } from "framer-motion";
 import { IoTime, IoSearch } from "react-icons/io5";
-import { FaCalendarAlt,FaBorderAll } from "react-icons/fa";
+import { FaCalendarAlt, FaBorderAll } from "react-icons/fa";
 import { PiHamburgerFill } from "react-icons/pi";
 import { LuCakeSlice } from "react-icons/lu";
 import { RiDrinksLine } from "react-icons/ri";
@@ -15,6 +15,7 @@ export default function Fetchadd() {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("Tümü");
   const [searchQuery, setSearchQuery] = useState("");
+  const [showContent, setShowContent] = useState(false);
   const { data, loading, fetchProduct } = useSupabase();
 
   // Tarih ve zaman ayarı
@@ -47,6 +48,13 @@ export default function Fetchadd() {
     return () => clearInterval(intervalId);
   }, []);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   // Kategorileri oluştur
   useEffect(() => {
     if (data.length > 0) {
@@ -74,7 +82,7 @@ export default function Fetchadd() {
     setFilteredProducts(filtered);
   }, [selectedCategory, searchQuery, data]);
 
-  if (loading)
+  if (loading || !showContent)
     return (
       <div className="absolute inset-0 grid place-content-center">
         <img
@@ -85,107 +93,138 @@ export default function Fetchadd() {
       </div>
     );
 
+  const fadeInVariant = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <div className="w-full h-full p-5 lg:p-10">
-    <div className="bg-[#fbfbfbf5] w-full h-full rounded-lg lg:rounded-2xl p-5 grid gap-5 grid-rows-[auto,auto,auto,1fr]">
-      {/* Tarih ve Saat */}
-      <div className="grid grid-cols-1 sm:grid-cols-[250px,10px,150px] gap-2 px-2">
-        <div className="bg-white flex items-center rounded-lg lg:rounded-2xl p-3 shadow-md">
-          <div className="bg-[#3c3b3ba6] p-2 flex justify-center items-center rounded-full">
-            <FaCalendarAlt fill="white" />
-          </div>
-          <span className="ml-3 text-gray-700 capitalize font-bold text-sm lg:text-base">
-            {currentDate}
-          </span>
-        </div>
-        <span className="hidden sm:flex items-center text-2xl">-</span>
-        <div className="bg-white flex items-center rounded-lg lg:rounded-2xl p-3 shadow-md">
-          <div className="bg-[#3c3b3ba6] p-2 flex justify-center items-center rounded-full">
-            <IoTime fill="white" />
-          </div>
-          <span className="ml-3 text-gray-700 font-bold text-sm lg:text-base">
-            {currentTime}
-          </span>
-        </div>
-      </div>
-
-      {/* Kategori Kutuları */}
-      <div className="flex space-x-4 p-2 w-full overflow-x-auto scrollbar-hide">
-        {categories.map((category, index) => (
-          <div
-            key={index}
-            onClick={() => setSelectedCategory(category.name)}
-            className={`bg-white flex-shrink-0 w-[30vw] sm:w-[130px] h-[130px] rounded-lg lg:rounded-2xl flex flex-col justify-between p-3 shadow-md cursor-pointer ${
-              selectedCategory === category.name ? "ring-2 ring-gray-500" : ""
-            }`}
-          >
-            {index === 0 && (
-              <FaBorderAll fill={`${selectedCategory === category.name ? "#121212" : "gray"}`} size={30} />   
-            )}
-            {index === 1 && (
-              <PiHamburgerFill fill={`${selectedCategory === category.name ? "#f97316" : "orange"}`} size={30} />   
-            )}
-            {index === 2 && (
-              <LuCakeSlice stroke="white" fill={`${selectedCategory === category.name ? "purple" : "pink"}`} size={30} />   
-            )}
-            {index === 3 && (
-              <RiDrinksLine fill={`${selectedCategory === category.name ? "#789DBC" : "#D9EAFD"}`} size={30} />   
-            )}
-
-            <span className="font-bold text-sm lg:text-base">{category.name}</span>
-            <span className="text-xs lg:text-sm">{category.count} Ürün Var</span>
-          </div>
-        ))}
-      </div>
-
-      {/* Arama Çubuğu */}
-      <div className="p-2">
-        <div className="bg-white w-full rounded-lg lg:rounded-2xl p-3 flex justify-between items-center shadow-md">
-          <input
-            type="text"
-            placeholder="Ürün adı ile arayın..."
-            className="focus:outline-none w-full text-sm lg:text-base"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <div className="bg-[#bb3838] p-2 rounded-full flex items-center justify-center">
-            <IoSearch fill="white" />
-          </div>
-        </div>
-      </div>
-
-      {/* Ürün Listesi */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-5 overflow-y-auto max-h-[600px] p-2">
-        {filteredProducts.map((item, index) => (
-          <div
-            key={index}
-            className="bg-white w-full h-[260px] rounded-lg lg:rounded-2xl grid grid-rows-[4fr,2fr,1fr] p-2 shadow-md"
-          >
-            {/* Resim Bölgesi */}
-            <div className="w-full h-full rounded-lg lg:rounded-2xl flex justify-center items-center overflow-hidden">
-              <img
-                src={item.image_url}
-                alt={item.name}
-                className="w-full h-full object-cover"
-              />
+    <motion.div 
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 1 }}
+      className="w-full h-full p-5 lg:p-10"
+    >
+      <motion.div
+        variants={fadeInVariant}
+        className="bg-[#fbfbfbf5] w-full h-full rounded-lg lg:rounded-2xl p-5 grid gap-5 grid-rows-[auto,auto,auto,1fr]"
+      >
+        {/* Tarih ve Saat */}
+        <motion.div 
+          variants={fadeInVariant} 
+          transition={{ delay: 0.1 }} 
+          className="grid grid-cols-1 sm:grid-cols-[250px,10px,150px] gap-2 px-2"
+        >
+          <div className="bg-white flex items-center rounded-lg lg:rounded-2xl p-3 shadow-md">
+            <div className="bg-[#3c3b3ba6] p-2 flex justify-center items-center rounded-full">
+              <FaCalendarAlt fill="white" />
             </div>
-
-            {/* Ürün Adı */}
-            <span className={`font-bold line-clamp-2 text-sm lg:text-base flex items-center`}>
-              {item.name}
+            <span className="ml-3 text-gray-700 capitalize font-bold text-sm lg:text-base">
+              {currentDate}
             </span>
+          </div>
+          <span className="hidden sm:flex items-center text-2xl">-</span>
+          <div className="bg-white flex items-center rounded-lg lg:rounded-2xl p-3 shadow-md">
+            <div className="bg-[#3c3b3ba6] p-2 flex justify-center items-center rounded-full">
+              <IoTime fill="white" />
+            </div>
+            <span className="ml-3 text-gray-700 font-bold text-sm lg:text-base">
+              {currentTime}
+            </span>
+          </div>
+        </motion.div>
 
-            {/* Kategori ve Buton */}
-            <div className="flex justify-between items-center">
-              <span className={`${item.category === "Sandviç" ? "text-orange-500" : item.category === "Tatlı" ? "text-[#800080]" : item.category === "İçecekler" ? "text-[#789dbc]" : ""} text-xs lg:text-sm font-semibold`}>
-                {item.category}
-              </span>
-              <button className="text-lg mr-2">+</button>
+        {/* Kategori Kutuları */}
+        <motion.div 
+          variants={fadeInVariant} 
+          transition={{ delay: 0.2 }} 
+          className="flex space-x-4 p-2 w-full overflow-x-auto scrollbar-hide"
+        >
+          {categories.map((category, index) => (
+            <div
+              key={index}
+              onClick={() => setSelectedCategory(category.name)}
+              className={`bg-white flex-shrink-0 w-[30vw] sm:w-[130px] h-[130px] rounded-lg lg:rounded-2xl flex flex-col justify-between p-3 shadow-md cursor-pointer ${
+                selectedCategory === category.name ? "ring-2 ring-gray-500" : ""
+              }`}
+            >
+              {index === 0 && (
+                <FaBorderAll fill={`${selectedCategory === category.name ? "#121212" : "gray"}`} size={30} />   
+              )}
+              {index === 1 && (
+                <PiHamburgerFill fill={`${selectedCategory === category.name ? "#f97316" : "orange"}`} size={30} />   
+              )}
+              {index === 2 && (
+                <LuCakeSlice stroke="white" fill={`${selectedCategory === category.name ? "purple" : "pink"}`} size={30} />   
+              )}
+              {index === 3 && (
+                <RiDrinksLine fill={`${selectedCategory === category.name ? "#789DBC" : "#D9EAFD"}`} size={30} />   
+              )}
+
+              <span className="font-bold text-sm lg:text-base">{category.name}</span>
+              <span className="text-xs lg:text-sm">{category.count} Ürün Var</span>
+            </div>
+          ))}
+        </motion.div>
+
+        {/* Arama Çubuğu */}
+        <motion.div 
+          variants={fadeInVariant} 
+          transition={{ delay: 0.3 }} 
+          className="p-2"
+        >
+          <div className="bg-white w-full rounded-lg lg:rounded-2xl p-3 flex justify-between items-center shadow-md">
+            <input
+              type="text"
+              placeholder="Ürün adı ile arayın..."
+              className="focus:outline-none w-full text-sm lg:text-base"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <div className="bg-[#bb3838] p-2 rounded-full flex items-center justify-center">
+              <IoSearch fill="white" />
             </div>
           </div>
-        ))}
-      </div>
-    </div>
-  </div>
+        </motion.div>
+
+        {/* Ürün Listesi */}
+        <motion.div 
+          variants={fadeInVariant} 
+          transition={{ delay: 0.4 }} 
+          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-5 overflow-y-auto max-h-[600px] p-2"
+        >
+          {filteredProducts.map((item, index) => (
+            <motion.div
+              key={index}
+              variants={fadeInVariant}
+              transition={{ delay: 0.5 + index * 0.1 }}
+              className="bg-white w-full h-[260px] rounded-lg lg:rounded-2xl grid grid-rows-[4fr,2fr,1fr] p-2 shadow-md"
+            >
+              {/* Resim Bölgesi */}
+              <div className="w-full h-full rounded-lg lg:rounded-2xl flex justify-center items-center overflow-hidden">
+                <img
+                  src={item.image_url}
+                  alt={item.name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+
+              {/* Ürün Adı */}
+              <span className={`font-bold line-clamp-2 text-sm lg:text-base flex items-center`}>
+                {item.name}
+              </span>
+
+              {/* Kategori ve Buton */}
+              <div className="flex justify-between items-center">
+                <span className={`${item.category === "Sandviç" ? "text-orange-500" : item.category === "Tatlı" ? "text-[#800080]" : item.category === "İçecekler" ? "text-[#789dbc]" : ""} text-xs lg:text-sm font-semibold`}>
+                  {item.category}
+                </span>
+                <button className="text-lg mr-2">+</button>
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
